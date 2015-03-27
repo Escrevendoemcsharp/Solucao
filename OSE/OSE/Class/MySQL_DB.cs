40497a488325fbf6300cef5afd5f9f7d1de7f4cb
Rafael Fernandes <rafael_mottarp@live.com>
2015-03-27 14:54:11 -0300

    OSE

---
+++ OSE/OSE/Class/MySQL_DB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace OSE.Class
+{   
+    /// <summary>
+    /// MySQL_DB Rafael Fernandes
+    /// </summary>
+    public partial class MySQL_DB
+    { 
+        
+        public bool Testar_Conexao_MySQL()
+        {
+            CConexao = new MySqlConnection(_Conexao_String_MySQL);
+            try
+            {
+                CConexao.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            if (CConexao.State == System.Data.ConnectionState.Open) { return true; }
+            else
+            {
+                return false;
+            }
+        }
+        #region DECLARE
+        private string _Conexao_String_MySQL 
+        {
+            get
+            { return
+                "SERVER=" + OSE_BASE.OSE_MySQL_Server +
+                ";DATABASE=" + OSE_BASE.OSE_MySQL_Database +
+                ";UID=" + OSE_BASE.OSE_MySQL_Uid +
+                ";PASSWORD=" +  OSE_BASE.OSE_MySQL_Password + ";";
+            }
+        }
+        private MySqlConnection CConexao;
+
+        #endregion
+    }
+}
---
+++ OSE/OSE/Class/OSE_BASE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSE.Class
+{
+    public class OSE_BASE
+    {
+        public string OSE_SOLUCAO_VERSAO { get { return " Master OSE 6.96.10 ©"; } }
+        public string OSE_SOLUCAO_ARQUIVO_CONFIG_XML { get { return _GetConfFile(); } }
+
+        #region Static
+        public static string OSE_MySQL_Server { get; set; }
+        public static string OSE_MySQL_Database { get; set; }
+        public static string OSE_MySQL_Uid { get; set; }
+        public static string OSE_MySQL_Password { get; set; }
+        public static string OSE_SOLUCAO_CHAVE_VERSAO { get; set; }
+        #endregion
+        static string _GetConfFile()
+        {
+            return
+                AppDomain.CurrentDomain.BaseDirectory + "\\OSE_SOLUCAO_CONFIG.XML";
+
+        }
+    }
+}
